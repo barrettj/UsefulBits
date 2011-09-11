5321cab6a7e0d12c2e520f88bb1208419e02c56f
Doug Russell <doug.russell@bottlerocketapps.com>
2011-09-11 13:12:40 -0500

    Macro for easy switching between building with and without arc

---
+++ Macros/ARCLogic.h
@@ -0,0 +1,31 @@
+//
+//  ARCLogic.h
+//	
+
+#if __has_feature(objc_arc)
+#define HASARC 1
+#else
+#define HASARC 0
+#endif
+
+#if __has_feature(objc_arc_weak)
+#define HASWEAK 1
+#else
+#define HASWEAK 0
+#endif
+
+#if HASARC
+#define IF_ARC(ARCBlock) ARCBlock
+#define NO_ARC(NoARCBlock) 
+#define STRONG strong
+#if HASWEAK
+#define WEAK weak
+#else
+#define WEAK assign
+#endif
+#else
+#define IF_ARC(ARCBlock) 
+#define NO_ARC(NoARCBlock) NoARCBlock
+#define STRONG retain
+#define WEAK assign
+#endif
